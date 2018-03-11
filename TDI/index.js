38a9bd1d2a76a3b58bb6018546a919a38d9ddf6d
Warren <warrenchan28@gmail.com>
2018-03-11 18:58:56 -0500

    ti

--- TDI/index.js
+++ TDI/index.js
@@ -259,8 +259,8 @@ class App extends React.Component {
 //      } else {
 //          return '91+';
 //      }
-      if (tdi < 0.2994058) return 'TDI-Inactive';
-      else if (tdi === 0) return 0;
+      
+      if (tdi === 0) return 0;
       else if (tdi <= -0.2597767) return 1;
       else if (tdi <= -0.136424) return 2;
       else if (tdi <= -0.0573148) return 3;
@@ -388,17 +388,17 @@ class App extends React.Component {
 //                elm('h1', null, this.formatValue(this.tdi))
 //              ]),
               elm('div', { id: 'tdiGroup', className: 'single-result'}, [
-                elm('h4', null, 'Transplant Death Index'),
+                elm('h4', null, 'Transplant Index'),
                 elm('h1', null, this.tdiGroup)
               ]),
-//              elm('div', { id: 'p-transplant', className: 'single-result'}, [
-//                elm('h4', null, 'pTransplant'),
-//                elm('h1', null, this.formatValue(this.transplantProb) + '%')
-//              ]),
-//              elm('div', { id: 'p-death', className: 'single-result'}, [
-//                elm('h4', null, 'pDeath'),
-//                elm('h1', null, this.formatValue(this.deathProb) + '%')
-//              ]),
+             elm('div', { id: 'p-transplant', className: 'single-result'}, [
+               elm('h4', null, 'pTransplant'),
+               elm('h1', null, this.formatValue(this.transplantProb) + '%')
+             ]),
+             elm('div', { id: 'p-death', className: 'single-result'}, [
+               elm('h4', null, 'pDeath'),
+               elm('h1', null, this.formatValue(this.deathProb) + '%')
+             ]),
 //              elm('div', { id: 'p-activity', className: 'single-result'}, [
 //                elm('h4', null, 'pActivity'),
 //                elm('h1', null, this.formatValue(this.activity) + '%')
